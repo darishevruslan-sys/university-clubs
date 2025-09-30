4c814bb5c6fe25346cee68217b54db12474027e8
darishevruslan-sys <darishev.ruslan@gmail.com>
2025-09-30 12:26:09 +0300

    Restore static routes for disabled pages

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,11 @@ import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Clubs from './pages/Clubs';
 import ClubDetail from './pages/ClubDetail';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Profile from './pages/Profile';
+import CreateClub from './pages/CreateClub';
+import EditClub from './pages/EditClub';
 import './App.css';
 
 function App() {
@@ -15,6 +20,12 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/clubs" element={<Clubs />} />
           <Route path="/clubs/:id" element={<ClubDetail />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/create-club" element={<CreateClub />} />
+          <Route path="/clubs/:id/edit" element={<EditClub />} />
+          <Route path="*" element={<Home />} />
         </Routes>
       </main>
     </div>
